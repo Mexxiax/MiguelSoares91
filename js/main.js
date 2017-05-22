bbe2484ed4cb0b95049f1a0aaaef1fed8aa4bc6a
Mexxiax <miguelsoares91@gmail.com>
2017-05-22 13:21:44 +0100

    aula 14

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 var likeButtonClicked = false;
 var dislikeButtonClicked = false;
-var liststats = new Array();
+var db = openDatabase('mydb', '1.0', 'Test DB', 2 * 1024 * 1024);
+db.transaction(function (tx) {
+	// tx.executeSql('DROP TABLE books');
+	tx.executeSql('CREATE TABLE IF NOT EXISTS books (id unique, opinion)');
+});
 
 var currsymb = {
 	'USD': '$', // US Dollar
@@ -21,32 +25,34 @@ var currsymb = {
 };
 function LoadDataWithHTML(book){
 	var HTMLtoInsert = `
-      <div class="book col-sm-10 col-sm-offset-1">
-        <img alt="Sem capa disponível">
-        <h1></h1>
-        <h3 style="display:inline;"></h3><a class="shop" target="blank"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span></a>
-        <p></p>
-        <h4>Editora:<label></label></h4><h5>Publicado em:<label></label></h5>
-        <br>
-        <br>
-          <button class="btn down dislike pull-left btn-danger" type="button" aria-label="left Align">
-            <span class="glyphicon glyphicon-thumbs-down" aria-hidden="true"></span>Não Gosto
-          </button>
-          <button class="btn up like pull-right btn-success" type="button" aria-label="left Align">
-            <span class="glyphicon glyphicon-thumbs-up" aria-hidden="true"></span>Gosto
-          </button>
-        </div>
-        </div>
-      </div>`;
+	<div class="book col-sm-10 col-sm-offset-1">
+	<img alt="Sem capa disponível">
+	<input type="hidden" class="hiddenFieldId"></input>
+	<h1></h1>
+	<h3 style="display:inline;"></h3><a class="shop" target="blank"><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span></a>
+	<p></p>
+	<h4>Editora:<label></label></h4><h5>Publicado em:<label></label></h5>
+	<br>
+	<br>
+	<button data-opinion="dislike" class="btn down dislike pull-left btn-danger" type="button" aria-label="left Align">
+	<span class="glyphicon glyphicon-thumbs-down" aria-hidden="true"></span>Não Gosto
+	</button>
+	<button data-opinion="like" class="btn up like pull-right btn-success" type="button" aria-label="left Align">
+	<span class="glyphicon glyphicon-thumbs-up" aria-hidden="true"></span>Gosto
+	</button>
+	</div>
+	</div>
+	</div>`;
 
 
-		$("#bookContainer").append(HTMLtoInsert);
-		$currentBook = $(".book").eq(-1);
-		$("h1",$currentBook).text(book.volumeInfo.title);
-		$("h4 label",$currentBook).text(book.volumeInfo.publisher);
-		$("h5 label",$currentBook).text(book.volumeInfo.publishedDate);
-		$("p",$currentBook).text(book.volumeInfo.description);	
-		if (typeof book.volumeInfo.imageLinks !== "undefined"){
+	$("#bookContainer").append(HTMLtoInsert);
+	$currentBook = $(".book").eq(-1);
+	$("h1",$currentBook).text(book.volumeInfo.title);
+	$('.hiddenFieldId',$currentBook).text(book.id);
+	$("h4 label",$currentBook).text(book.volumeInfo.publisher);
+	$("h5 label",$currentBook).text(book.volumeInfo.publishedDate);
+	$("p",$currentBook).text(book.volumeInfo.description);	
+	if (typeof book.volumeInfo.imageLinks !== "undefined"){
 			//$("img",$currentBook).attr("alt","Sem capa disponível").css("color","#B22222");
 			$("img",$currentBook).attr("src",book.volumeInfo.imageLinks.thumbnail);
 		}
@@ -62,92 +68,101 @@ function LoadDataWithHTML(book){
 		}
 
 
-}
-
-var APIKey ="AIzaSyAe6Pmvy0F37CzEUl6pFW5UbQ9PBPy9A_Q";
-var UserID = "109471736623302330343";
-var ShelfID = "1001";
-
-$.ajax({
-	url:"https://www.googleapis.com/books/v1/users/" + UserID + "/bookshelves/" + ShelfID + "/volumes?key=" + APIKey + "&maxResults=40"
-	
-
-}).done(function(data){
-	$.each(data.items,function(index,value){
-		console.log(value);
-		LoadDataWithHTML(value);
-	});
-	$(".book").eq(0).addClass("active");
-});
-$("#moldura button.upagina").click(function(){
-	// $allBooks = $(".book");
-	// $parent = $(".book.active");
-		$("#moldura").hide()
-		$("#stats").show();
-});
-
-var inAnimation = false;
-$("#moldura button.previous").click(function(){
-	$allBooks = $(".book");
-	$parent = $(".book.active");
-	var index = $allBooks.index($parent);
-
-	if(!inAnimation && index > 0){
-		inAnimation = true;
-		$previous = $parent.prev(".book");
-
-		if( index >= $allBooks.length-1){
-			$previous = $allBooks.eq(0);
-		}
-		$parent.fadeOut(100,function(){
-			$parent.removeClass("active");
-			$previous.fadeIn(100,function(){
-				$previous.addClass("active");
-				inAnimation = false;
-			})
-		})
-	};
-});
-var inAnimation = false;
-$("#moldura button.next").click(function(){
-	likeButtonClicked = false;
-	dislikeButtonClicked = false;
-	if(!inAnimation){
-		inAnimation = true;
-	$allBooks = $(".book");
-	$parent = $(".book.active");
-	
-	
-	var index = $allBooks.index($parent);
-	$next = $parent.next(".book");
-
-	if( index >= $allBooks.length-1){
-		$next = $allBooks.eq(0);
-		$("#moldura").hide()
-		$("#stats").show();
-
 	}
-	$parent.fadeOut(100,function(){
-		$parent.removeClass("active");
-		$next.fadeIn(100,function(){
-			$next.addClass("active");
-			inAnimation = false;
-		})
-	})
-};
-});
 
-var count = 0;
-$("#stats label.countlike").text("Gosto: "+count);
-$("body").on("click",".book button.like", function(){
-	if(!likeButtonClicked){
-		likeButtonClicked = true;
-		count++;
-		$("#stats label.countlike").text("Gosto: "+count);
-		$(".dislike",$(".book.active")).hide();
-		$(".like",$(".book.active")).removeClass("pull-right");
-		$(".like",$(".book.active")).addClass("liked");
-	}else{
+	var APIKey ="AIzaSyAe6Pmvy0F37CzEUl6pFW5UbQ9PBPy9A_Q";
+	var UserID = "109471736623302330343";
+	var ShelfID = "1001";
+
+	$.ajax({
+		url:"https://www.googleapis.com/books/v1/users/" + UserID + "/bookshelves/" + ShelfID + "/volumes?key=" + APIKey + "&maxResults=40"
+
+
+	}).done(function(data){
+		$.each(data.items,function(index,value){
+			console.log(value);
+			LoadDataWithHTML(value);
+		});
+		$(".book").eq(0).addClass("active");
+	});
+
+
+	var inAnimation = false;
+	$("#moldura button.previous").click(function(){
+		$allBooks = $(".book");
+		$parent = $(".book.active");
+		var index = $allBooks.index($parent);
+
+		if(!inAnimation && index > 0){
+			inAnimation = true;
+			$previous = $parent.prev(".book");
+
+			if( index >= $allBooks.length-1){
+				$previous = $allBooks.eq(0);
+			}
+			$parent.fadeOut(100,function(){
+				$parent.removeClass("active");
+				$previous.fadeIn(100,function(){
+					$previous.addClass("active");
+					inAnimation = false;
+				})
+			})
+		};
+	});
+	var inAnimation = false;
+	$("#moldura button.next").click(function(){
+		likeButtonClicked = false;
+		dislikeButtonClicked = false;
+		if(!inAnimation){
+			inAnimation = true;
+			$allBooks = $(".book");
+			$parent = $(".book.active");
+
+
+			var index = $allBooks.index($parent);
+			$next = $parent.next(".book");
+
+			if( index >= $allBooks.length-1){
+				$next = $allBooks.eq(0);
+				$("#moldura").hide()
+				$("#stats").show();
+
+			}
+			$parent.fadeOut(100,function(){
+				$parent.removeClass("active");
+				$next.fadeIn(100,function(){
+					$next.addClass("active");
+					inAnimation = false;
+				})
+			})
+		};
+	});
+
+	var count = 0;
+	$("#stats label.countlike").text("Gosto: "+count);
+
+
+
+	$("body").on("click",".book button.like", function(){
+		if(!likeButtonClicked){
+			likeButtonClicked = true;
+			count++;
+			$parent = $(".book.active");
+			$("#stats label.countlike").text("Gosto: "+count);
+			$(".dislike",$(".book.active")).hide();
+			$(".like",$(".book.active")).removeClass("pull-right");
+			$(".like",$(".book.active")).addClass("liked");
+			$id = $('.hiddenFieldId',$parent).text();
+
+		// vamos buscar a opinion ao nosso custom attribute
+		$opinion = $(this).attr('data-opinion');
+
+		db.transaction(function (tx) {
+			//insert na table que criámos
+			tx.executeSql('INSERT INTO books(id, opinion) VALUES("' + $id + '","' + $opinion + '")');
+		});
+	}
+	else{
 		likeButtonClicked = false;
 		count--;
 		$("#stats label.countlike").text("Gosto: "+count);
@@ -157,39 +172,63 @@ $("body").on("click",".book button.like", function(){
 	}
 	
 });
-var counte = 0;
-$("#stats label.countdislike").text("Não Gosto: "+counte);
-$("body").on("click",".book button.dislike", function(){
-	if(!dislikeButtonClicked){
-		dislikeButtonClicked = true;
-		counte++;
+	var counte = 0;
 	$("#stats label.countdislike").text("Não Gosto: "+counte);
-	$(".like",$(".book.active")).hide();
-	$(".dislike",$(".book.active")).removeClass("pull-left");
-	$(".dislike",$(".book.active")).addClass("disliked");
-}
-else{
+
+
+
+
+
+
+
+
+	$("body").on("click",".book button.dislike", function(){
+		if(!dislikeButtonClicked){
+			dislikeButtonClicked = true;
+			counte++;
+			$parent = $(".book.active");
+			$("#stats label.countdislike").text("Não Gosto: "+counte);
+			$(".like",$(".book.active")).hide();
+			$(".dislike",$(".book.active")).removeClass("pull-left");
+			$(".dislike",$(".book.active")).addClass("disliked");
+			$id = $('.hiddenFieldId',$parent).text();
+
+		// vamos buscar a opinion ao nosso custom attribute
+		$opinion = $(this).attr('data-opinion');
+
+		db.transaction(function (tx) {
+			//insert na table que criámos
+			tx.executeSql('INSERT INTO books(id, opinion) VALUES("' + $id + '","' + $opinion + '")');
+		});
+	}
+	else{
 		dislikeButtonClicked = false;
 		counte--;
 		$("#stats label.countdislike").text("Não Gosto: "+counte);
 		$(".like",$(".book.active")).show();
 		$(".dislike",$(".book.active")).addClass("pull-left");
 		$(".dislike",$(".book.active")).removeClass("disliked");
+
 	}
 });
-function loadstats(book){
-var insertstats = `
-<div class="book col-sm-10 col-sm-offset-1">
-<h3></h3>
-</div>`;
 
-$("#stats").append(insertstats);
-$statbook = $(".liked").eq(0);
-$("h3",$statbook).text(book.volumeInfo.title);
-}
-
-loadstats(book);
-
+	$("#moldura button.upagina").click(function(){
+	// $allBooks = $(".book");
+	// $parent = $(".book.active");
+	$("#moldura").hide()
+	$("#stats").show();
+	db.transaction(function (tx) {
+			//buscar todos os resultados da nossa table
+			tx.executeSql('SELECT * FROM books', [], function (tx, results) {
+				$.each(results.rows,function(index,item){
+		   			//output de todas as rows/todos os resultados
+					// $statsbook = $("#stats")
+					var html = `<p>`+ item.opinion + item.id +`</p>`;
+					$("#stats h3").append(html);
+				});
+			}, null);
+		});
+	});
 
 
 
